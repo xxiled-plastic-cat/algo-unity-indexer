866cbd90b6efb48fe5448a00ddb158146bb8dce2
Kieran Nelson <kieran.nelson@aurora-digital.com>
2023-03-15 11:34:27 +0000

    initial 2

---
+++ algo-unity/algo-unity/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        HttpClient httpClient = new HttpClient();
+        var response = httpClient.GetAsync("https://mainnet-idx.algonode.cloud/v2/accounts/RS7TLLQRXKBAQDAVTSZC2ZLMVMLNSCL3FOUOESJJZ5XSKFFL56UI6X33CI/assets").Result;
+        response.EnsureSuccessStatusCode();
+        string responseBody = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(responseBody);
+    }
+
+}
